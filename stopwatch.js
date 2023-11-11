4a6fa70e4c7d1286d6575ab9399f724bf35b5df8
zahrabateninia <zahrabatenin@gmail.com>
2023-11-11 21:46:27 +0330

    Define properties and methods in the constructor function

---
+++ stopwatch.js
@@ -0,0 +1,15 @@
+function Stopwatch(){
+    let duration, startTime, endTime, running = 0;
+    this.start = function(){
+
+    };
+    this.stop = function(){
+
+    };
+    this.reset()= function(){
+
+    };
+  
+
+};
+
