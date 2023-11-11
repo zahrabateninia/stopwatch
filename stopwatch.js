64744f1d49c36b915d764263be9757e62d225016
zahrabateninia <zahrabatenin@gmail.com>
2023-11-11 23:17:03 +0330

    Throw error message in certain conditions
    if the user wanted to start, when the process was alreadt started, an error will be thrown,
    and if he/she tries to stop it when it's not started yet, also an error will be shown

--- stopwatch.js
+++ stopwatch.js
@@ -1,9 +1,16 @@
 function Stopwatch(){
     let duration, startTime, endTime, running = 0;
     this.start = function(){
-
+        if (running)
+            throw new Error("The stopwatch has already started");
+        running = true;
+        startTime = new Date();
     };
     this.stop = function(){
+        if (!running)
+            throw new Error("The stopwatch is not running");
+        running = false;
+        endTime = new Date();
 
     };
     this.reset()= function(){
